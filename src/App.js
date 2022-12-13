33d4a7547dbce649c64ecd31beb62ef86bb16301
Minh_Nexle <minhtd@nexle.com>
2022-12-13 17:30:55 +0700

    Finish scroll

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-
 import InfiniteScroll from "react-infinite-scroll-component";
 import axios from "axios";
 import "./App.css";
@@ -8,24 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 import Box from "@mui/material/Box";
 import { Container } from "@mui/material";
 import Loader from "./components/Loader";
-import Card from "@mui/material/Card";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
 import Post from "./components/Post";
-// import { makeStyles } from "@material-ui/core/styles";
-
-// const useStyles = makeStyles({
-//   root: {
-//     background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-//     border: 0,
-//     borderRadius: 3,
-//     boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-//     color: "white",
-//     height: 48,
-//     padding: "0 30px",
-//   },
-// });
 
 function App() {
   const [id, setId] = useState(2);
@@ -73,6 +55,8 @@ function App() {
             "Content-Type": "application/json",
           },
         });
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
         setListItems(data.nodes);
       } catch (error) {
         toast.error("Unable to load post !", {
@@ -90,12 +74,11 @@ function App() {
         <Box
           component="div"
           sx={{
-            bgcolor: "#cfe8fc",
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
-            paddingTop: "14px",
-            paddingBottom: "14px",
+            pt: 2,
+            pb: 2,
           }}
         >
           <InfiniteScroll
@@ -103,58 +86,13 @@ function App() {
             next={fetchData}
             hasMore={hasMore}
             loader={<Loader />}
-            endMessage={
-              <p style={{ textAlign: "center" }}>
-                <b>Yay! You have seen it all post</b>
-              </p>
-            }
           >
-            {listItems?.length ? (
-              listItems.map((post, index) => {
-                return <Post post={post} />;
-              })
-            ) : (
-              <></>
-            )}
+            {listItems.map((post) => {
+              return <Post post={post} key={post.node.nid} />;
+            })}
           </InfiniteScroll>
         </Box>
       </Container>
-      {/* <div className="app">
-        <div>
-          <InfiniteScroll
-            dataLength={listItems.length}
-            next={fetchData}
-            hasMore={hasMore}
-            loader={<h4 className="content__center">Loading post...</h4>}
-            endMessage={
-              <p style={{ textAlign: "center" }}>
-                <b>Yay! You have seen it all post</b>
-              </p>
-            }
-          >
-            {listItems.map((data, index) => {
-              return (
-                <div key={index} className="wrapper__item">
-                  <div className="wrapper__item--img">
-                    <img
-                      src={data.node.field_photo_image_section}
-                      alt="images"
-                    />
-                  </div>
-                  <div className="wrapper__item--text">
-                    <h1 className="wrapper__item--text--main">
-                      {data.node?.title ?? ""}
-                    </h1>
-                    <p className="wrapper__item--text--time">
-                      {moment(data.node.last_update).format("ll, h:mm A")} IST
-                    </p>
-                  </div>
-                </div>
-              );
-            })}
-          </InfiniteScroll>
-        </div>
-      </div> */}
       <ToastContainer />
     </>
   );
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,44 +1,47 @@
-import { Card, Box, CardMedia, CardContent, Typography } from "@mui/material";
+import { Card, Box, CardContent, Typography } from "@mui/material";
 import moment from "moment";
 
 const Post = ({ post }) => {
   return (
-    <Card sx={{ display: "flex" }}>
-      <Box sx={{ flex: "1" }} mb={2}>
-        <CardMedia
-          sx={{ borderRadius: "20px" }}
-          component="img"
-          height="100%"
-          width="100%"
-          image={post.node.field_photo_image_section}
+    <Card
+      sx={{
+        display: "flex",
+        height: { xs: "20vh", sm: "30vh", md: "40vh" },
+        boxShadow: "none",
+      }}
+    >
+      <Box sx={{ flexBasis: "40%" }} mb={2}>
+        <img
+          src={post.node.field_photo_image_section}
           alt="images"
+          loading="lazy"
         />
       </Box>
-      <CardContent sx={{ flex: "1" }}>
+      <CardContent sx={{ flexBasis: "60%" }}>
         <Typography
           variant="h5"
-          color="text.secondary"
           sx={{
             overflow: "hidden",
             textOverflow: "ellipsis",
             display: "-webkit-box",
-            "-webkit-line-clamp": "3",
-            "line-clamp": "2",
-            "-webkit-box-orient": "vertical",
+            WebkitLineClamp: "3",
+            lineClamp: "2",
+            WebkitBoxOrient: "vertical",
+            fontSize: { xs: 15, sm: 20, md: 20 },
           }}
         >
           {post?.node?.title ?? ""}
         </Typography>
         <Typography
           variant="body1"
-          color="text.secondary"
           sx={{
-            marginTop: "10px",
-            color: "gray",
+            mt: { xs: 0.5, sm: 1, md: 1 },
+            color: "#424242",
+            fontSize: { xs: 10, sm: 18, md: 20 },
           }}
         >
           {post?.node?.last_update
-            ? `${moment(post.node.last_update).format("ll, h:mm A")} IST`
+            ? `${moment(post.node.last_update).format("ll h:mm A")} IST`
             : ""}
         </Typography>
       </CardContent>
